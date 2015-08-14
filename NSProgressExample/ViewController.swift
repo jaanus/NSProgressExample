6b7f48d8f192df228764e127e999a8dcb5ab5c9a
Jaanus Kase <jaanus@gmail.com>
2015-08-14 13:16:33 +0200

    Initial Commit

---
+++ NSProgressExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSProgressExample
+//
+//  Created by Jaanus Kase on 14/08/15.
+//  Copyright © 2015 Jaanus Kase. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
