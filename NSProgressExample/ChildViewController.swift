e3234c4ac8e54aea97f5fa7af741909bca5887eb
Jaanus Kase <jaanus@gmail.com>
2015-08-14 13:58:24 +0200

    Custom child VC class

---
+++ NSProgressExample/ChildViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChildViewController.swift
+//  NSProgressExample
+//
+//  Created by Jaanus Kase on 14/08/15.
+//  Copyright © 2015 Jaanus Kase. All rights reserved.
+//
+
+import Cocoa
+
+class ChildViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
