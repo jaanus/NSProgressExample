3cb221f3de392e04091ae699ecef1e46de815028
jaanus <jaanus@gmail.com>
2015-08-20 16:31:20 +0200

    Child progress indication somewhat works.

--- NSProgressExample/ChildViewController.swift
+++ NSProgressExample/ChildViewController.swift
@@ -8,10 +8,10 @@
 
 import Cocoa
 
-
-
 class ChildViewController: NSViewController, NSProgressReporting, ProgressSheetInterface {
 
+    @IBOutlet weak var statusLabel: NSTextField!
+    
     // NSProgressReporting
     var progress = NSProgress()
     
@@ -25,13 +25,13 @@ class ChildViewController: NSViewController, NSProgressReporting, ProgressSheetI
     }
     
     override func viewDidAppear() {
+        
+        longComputationTask()
         self.performSegueWithIdentifier("presentProgressSheetFromChild", sender: self)
         
         delay(2, closure: {
             print("oh hai")
-            // Not sure how to efficiently reverse-segue in Cocoa, so let’s just do it the hardcoded way.
-            // This assumes that the only possible presented sheet is the progress sheet.
-            self.dismissViewController((self.presentedViewControllers?.first)!)
+
         })
         
     }
@@ -58,5 +58,49 @@ class ChildViewController: NSViewController, NSProgressReporting, ProgressSheetI
                 Int64(delay * Double(NSEC_PER_SEC))
             ),
             dispatch_get_main_queue(), closure)
+    }
+    
+    func longComputationTask() {
+        let iterationLength = Float(0.05)
+        let taskDuration = Float(3)
+        let iterationCount: Int64 = Int64(taskDuration / iterationLength)
+        
+        progress = NSProgress(totalUnitCount: iterationCount)
+        progress.cancellationHandler = {
+            self.taskFinished(cancelled: true)
+        }
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+
+            for var i : Int64 = self.progress.completedUnitCount; i < iterationCount; i++ {
+                guard !self.progress.cancelled else { return }
+                guard !self.progress.paused else { return }
+                
+                NSThread.sleepForTimeInterval(NSTimeInterval(iterationLength))
+                
+                self.progress.completedUnitCount++
+            }
+            
+            self.progress.cancellationHandler = nil
+            self.taskFinished(cancelled: false)
+
+        }
+        
+    }
+    
+    func taskFinished(cancelled cancelled: Bool) {
+        dispatch_async(dispatch_get_main_queue()) {
+            
+            // Not sure how to efficiently reverse-segue in Cocoa, so let’s just do it the hardcoded way.
+            // This assumes that the only possible presented sheet is the progress sheet.
+            self.dismissViewController((self.presentedViewControllers?.first)!)
+            
+            if cancelled {
+                self.statusLabel.stringValue = "The task was cancelled."
+            } else {
+                self.statusLabel.stringValue = "The answer is 42."
+            }
+            
+        }
     }
 }
--- NSProgressExample/ProgressSheetViewController.swift
+++ NSProgressExample/ProgressSheetViewController.swift
@@ -8,6 +8,8 @@
 
 import Cocoa
 
+private var progressObservationContext = 0
+
 protocol ProgressSheetInterface {
     var sheetUserInteractive: Bool { get }
     var sheetLabel: String? { get }
@@ -18,17 +20,25 @@ class ProgressSheetViewController: NSViewController {
     @IBOutlet weak var controlsContainerView: NSStackView!
     @IBOutlet weak var sheetLabel: NSTextField!
     @IBOutlet weak var controlsContainerBottomMarginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var progressIndicatorView: NSProgressIndicator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
-        let progressSource = self.presentingViewController as! NSProgressReporting
+
         setupInterface()
+        setupProgressObserver()
+    }
+    
+    deinit {
+        if let progressSource = self.presentingViewController {
+            progressSource.removeObserver(self, forKeyPath: "progress.fractionCompleted")
+        }
     }
 
     func setupInterface() {
         if let interfaceSource = self.presentingViewController as? ProgressSheetInterface {
             if !interfaceSource.sheetUserInteractive {
+                
                 // if the buttons are not to be shown, hide them,
                 // and also deactivate the constraint that is holding them in place,
                 // so that the bottom margin of the progress bar kicks in
@@ -39,7 +49,29 @@ class ProgressSheetViewController: NSViewController {
             if let interfaceLabel = interfaceSource.sheetLabel {
                 sheetLabel.stringValue = interfaceLabel
             }
-            
+
+        }
+    }
+    
+    func setupProgressObserver() {
+        if let progressSource = self.presentingViewController {
+            progressSource.addObserver(self, forKeyPath: "progress.fractionCompleted", options: [], context: &progressObservationContext)
+        }
+    }
+    
+    
+    
+    // MARK: Key-Value Observing
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String: AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        guard context == &progressObservationContext else {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            let progressSource = self.presentingViewController as! NSProgressReporting
+            self.progressIndicatorView.doubleValue = progressSource.progress.fractionCompleted
         }
     }
     
